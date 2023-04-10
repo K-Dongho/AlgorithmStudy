ba7bc6ade52e64844074b378e34a06e3f5bc1be2
K-Dongho <kdongho9501@gmail.com>
2023-04-10 20:06:18 +0900

    1이될때까지

--- Greedy/_1이될때까지/Main.java
+++ Greedy/_1이될때까지/Main.java
@@ -1,2 +1,24 @@
-package Greedy._1이될때까지;public class Main {
+package Greedy._1이될때까지;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+
+        while (n != 1) { //1이 될때까지 1을 빼거나 n을 k로 나눔
+            if (n % k != 0) {
+                n--;
+                count++;
+            } else {
+                n /= k;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
--- Greedy/_1이될때까지/Main2.java
+++ Greedy/_1이될때까지/Main2.java
@@ -1,2 +1,25 @@
-package Greedy._1이될때까지;public class Main2 {
+package Greedy._1이될때까지;
+
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //n이 k의 배수가 되도록 한번에 빼기
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+
+        while (true) {
+            int target = (n / k) * k; //k로 나누어 떨어지는 가장 가까운수
+            count += (n - target);//한번에 빼기
+            n = target;
+            if (n < k) break;
+            count++;
+            n /= k;//k로 나누기
+        }
+        count += (n - 1);
+        System.out.println(count);
+    }
 }
